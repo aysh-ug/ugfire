9023a20ece63f65ed237dd124b68abe1667fc5ac
Ochola Alfred <59102788+aysh-ug@users.noreply.github.com>
2020-05-16 13:21:41 +0300

    Animated.js

---
+++ animated.js
@@ -0,0 +1,46 @@
+/*
+function function_name(x) {
+	// body...
+
+if (x.matches) {
+var tl = anime.timeline({
+  easing: '#redbox',
+  duration: 1000
+});
+
+
+var myAnimation = anime({
+  targets: '#redbox',
+  delay:1000,
+  translateX: 300, duration: 1000,
+  direction: 'alternate',
+  loop: true,
+  easing: 'linear'
+ 
+});
+}
+else{
+
+}
+}
+
+var x = window.matchmedia("(min-width:700px)")
+myFunction(x)
+x.addListener(myFunction)
+
+*/
+var tl = anime.timeline({
+  easing: '#redbox',
+  duration: 1000
+});
+
+
+var myAnimation = anime({
+  targets: '#redbox',
+  delay:1000,
+  translateX: 300, duration: 1000,
+  direction: 'alternate',
+  loop: true,
+  easing: 'linear'
+ 
+});
